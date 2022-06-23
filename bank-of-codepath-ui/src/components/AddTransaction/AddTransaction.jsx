52eabfec8363f5f4f3c09f82da6ab71f99e7a642
OsaruElaiho <Osaruelaiho@gmail.com>
2022-06-22 19:08:59 -0500

    v1

--- bank-of-codepath-ui/src/components/AddTransaction/AddTransaction.jsx
+++ bank-of-codepath-ui/src/components/AddTransaction/AddTransaction.jsx
@@ -1,34 +1,64 @@
 import * as React from "react"
 import "./AddTransaction.css"
 
-export default function AddTransaction() {
+export default function AddTransaction(props) {
+  function handleOnFormFieldChange (e) {
+   if(e.target.name == "description"){
+    props.setForm(props.from.description)
+   } else if(e.target.name == "category"){
+    props.setForm(props.form.category)
+   } else{
+    props.setForm(props.form.amount)
+   }
+  }
+
   return (
     <div className="add-transaction">
       <h2>Add Transaction</h2>
 
-      <AddTransactionForm />
+      <AddTransactionForm 
+      //handleOnFormFieldChange={handleOnFormFieldChange()}
+      />
     </div>
   )
 }
 
-export function AddTransactionForm() {
+export function AddTransactionForm(props) {
   return (
     <div className="form">
       <div className="fields">
         <div className="field">
           <label>Description</label>
-          <input />
+          <input 
+          name="description"
+          placeholder="choose description"
+          type={"text"}
+          value={props.description}
+           //onChange={props.handleOnFormFieldChange()}
+          />
         </div>
         <div className="field">
           <label>Category</label>
-          <input />
+          <input 
+          name="category"  
+          placeholder="choose category"
+          type={"text"}
+          value={props.category}
+          // onChange={props.handleOnFormFieldChange()}
+          />
         </div>
         <div className="field half-flex">
           <label>Amount (cents)</label>
-          <input />
+          <input 
+          name="amount"  
+          placeholder="choose amount"
+          type={"number"}
+          value={props.amount}
+           // onChange={props.handleOnFormFieldChange()}
+          />
         </div>
 
-        <button className="btn add-transaction" type="submit">
+        <button className="btn add-transaction" type="submit" onClick={() => handleOnFormFieldChange()}>
           Add
         </button>
       </div>
--- bank-of-codepath-ui/src/components/Home/Home.jsx
+++ bank-of-codepath-ui/src/components/Home/Home.jsx
@@ -2,12 +2,56 @@ import * as React from "react"
 import AddTransaction from "../AddTransaction/AddTransaction"
 import BankActivity from "../BankActivity/BankActivity"
 import "./Home.css"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
-export default function Home() {
+export default function Home(props) {
+  useEffect(() => {
+    const fetchData = async() => {
+      props.setIsLoading(true);
+      await axios.get("http://localhost:3001/bank/transactions")
+      .then(res => {
+        console.log("transactions", res.data.transactions)
+        props.setTransactions(res.data.transactions);
+        props.setIsLoading(false);
+      }).catch( err=> {
+        props.setError(err);
+      })
+      await axios.get("http://localhost:3001/bank/transfers")
+      .then( res=> {
+        props.setTransfers(res.data.transfers);
+        props.setIsLoading(false);
+      }).catch(err => {
+        props.setError(err);
+      })
+    }
+    fetchData();
+    },[])
+
+    // const transactionsArr = props.transactions
+    // Filtering transactions
+    const filteredTransactions = props.filterInputValue ? 
+    props.transactions.filter((currTransaction) => currTransaction.description.toLowerCase().indexOf(props.filterInputValue.toLowerCase()) !== -1)
+    : props.transactions
+    // should filter the transactions based on whether or not the lowercased description 
+    // property of a transaction contains(indexOf) the lowercased filterInputValue
+
+    function handleOnCreateTransaction () {
+
+    }
   return (
     <div className="home">
-      <AddTransaction />
-      <BankActivity />
+      <AddTransaction 
+        isCreating={props.isCreating} 
+        setIsCreating={props.setIsCreating} 
+        form={props.newTransactionForm}
+        setForm={props.setNewTransactionForm}
+        handleOnSubmit={handleOnCreateTransaction}
+      />
+      {props.isLoading ? 
+      (<h1>Loading...</h1>) : 
+      (<BankActivity transactions={filteredTransactions} />)}
+      {props.error ? (<h2>{props.error}</h2>) : (null)}
     </div>
   )
 }
